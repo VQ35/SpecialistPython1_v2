3ffc29298e941dce63b06b84281364deaf338c1f
VQ35 <133659089+VQ35@users.noreply.github.com>
2023-05-18 14:19:10 +0300

    Update 01_task_func.py

--- Module6/practice/funcs_part1/01_task_func.py
+++ Module6/practice/funcs_part1/01_task_func.py
@@ -11,3 +11,15 @@ print(max4(-10, -12, -4, -9))
 print(max4(2.5, 2.6, 2.6, 2.4))
 print(max4(-2.5, 0, -1.2, -0.4))
 print(max4(-2, -2, -2, -2))
+
+
+def max4(n1,n2,n3,n4):
+    numbers = [n1,n2,n3,n4]
+    max_i=n1
+    for num in numbers:
+            if max_i<num:
+                max_i=num
+    return max_i
+
+z=max4(10,12,3,4)
+print(z)
