e868558044221f1e568e526181858e1893fc821c
VQ35 <133659089+VQ35@users.noreply.github.com>
2023-05-18 08:26:50 +0300

    Update [hw]01_task_string.py

--- Module4/home_work/01_string/[hw]01_task_string.py
+++ Module4/home_work/01_string/[hw]01_task_string.py
@@ -1,4 +1,17 @@
 # Дан произвольный текст. Каких символов больше, точек "." или запятых ","?
 # Если точек и запятых одинаково - выведите "одинаково"
+str=(input("str= "))
 
+
+count_t=str.count(".")
+count_z=str.count(",")
+
+if count_t>count_z:
+    print("точек больше")
+elif count_z>count_t:
+    print("запятых больше")
+elif count_z==0 and  count_t==0:
+    print("точка и запятая отсутствуют")
+else:
+    print("одинаково")
 text = ...
