a69b71a3ea159287251bcb9aabfc9f738939cab0
VQ35 <133659089+VQ35@users.noreply.github.com>
2023-05-18 12:29:24 +0300

    Update 01_my_abs.py

--- Module6/practice/funcs_part0/01_my_abs.py
+++ Module6/practice/funcs_part0/01_my_abs.py
@@ -6,3 +6,19 @@ def my_abs(...):
 print(my_abs(-5))
 print(my_abs(5))
 print(my_abs(0))
+
+def my_abs(a):
+    if a>0:
+        return a
+    else:
+        return a*(-1)
+rez=my_abs(-5)
+print(rez)
+
+def my_abs(a):
+    if a>=0:
+        return a
+    else:
+        return a*(-1)
+rez=my_abs(0)
+print(rez)
